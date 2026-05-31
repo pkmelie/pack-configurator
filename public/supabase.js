76d1f25a4cd8f64a16f94cb00b304a789e9b47b8
pkmelie <elielululeo@gmail.com>
2026-05-31 19:53:28 +0200

    test

--- public/supabase.js
+++ public/supabase.js
@@ -6,7 +6,8 @@ script.onload = () => {
     // 2. Initialiser le client
     const supabaseUrl = 'https://jefxkfkwrdzagbeptyqo.supabase.co'; 
     const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImplZnhrZmt3cmR6YWdiZXB0eXFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMzczOTMsImV4cCI6MjA4ODkxMzM5M30.4w42OUP_yBTeT98BjDhY88wQKCFbF8djXg1kBcVOVRw';
-    window.supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+    // Remplace ton ancienne ligne par celle-ci :
+    window.supabase = supabase.createClient(supabaseUrl, supabaseKey);
     
     // Déclencher un événement quand Supabase est prêt
     document.dispatchEvent(new Event('supabaseReady'));
